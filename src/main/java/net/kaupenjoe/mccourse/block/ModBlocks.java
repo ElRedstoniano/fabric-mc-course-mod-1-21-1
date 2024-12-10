1421bcfe16388d7012ed99c0f8247412148dd61d
ElRedstoniano <42276499+ElRedstoniano@users.noreply.github.com>
2024-12-10 17:07:33 +0000

    Añadiendo más bloques simples custom (minerales del nether y end)

--- src/main/java/net/kaupenjoe/mccourse/block/ModBlocks.java
+++ src/main/java/net/kaupenjoe/mccourse/block/ModBlocks.java
@@ -27,6 +27,14 @@ public class ModBlocks {
             new ExperienceDroppingBlock(UniformIntProvider.create(3,6),
                     AbstractBlock.Settings.create()
                             .strength(6f).requiresTool()));
+    public static final Block FLUORITE_NETHER_ORE = registerBlock("fluorite_nether_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(4,6),
+                    AbstractBlock.Settings.create()
+                            .strength(3f).requiresTool()));
+    public static final Block FLUORITE_END_ORE = registerBlock("fluorite_end_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(5,7),
+                    AbstractBlock.Settings.create()
+                            .strength(5f).requiresTool()));
 
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
