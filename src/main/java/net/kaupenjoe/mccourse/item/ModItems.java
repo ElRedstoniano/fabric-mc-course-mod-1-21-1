28cea228465e0f6b6c0093da4396ca7905a835af
ElRedstoniano <42276499+ElRedstoniano@users.noreply.github.com>
2025-01-01 21:53:44 +0000

    Item custom avanzado - motosiera

--- src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
+++ src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
@@ -3,6 +3,7 @@ package net.kaupenjoe.mccourse.item;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.kaupenjoe.mccourse.MCCourseMod;
+import net.kaupenjoe.mccourse.item.custom.ChainSawItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -13,6 +14,9 @@ public class ModItems {
 
     public static final Item FLUORITE = registerItem("fluorite", new Item(new Item.Settings()));
     public static final Item RAW_FLUORITE = registerItem("raw_fluorite", new Item(new Item.Settings()));
+
+    public static final Item CHAINSAW = registerItem("chainsaw",
+            new ChainSawItem(new Item.Settings().maxDamage(32)));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM,
