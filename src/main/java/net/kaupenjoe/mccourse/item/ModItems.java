9dabe31a58025a4a49ea7b86902c242e343be674
ElRedstoniano <42276499+ElRedstoniano@users.noreply.github.com>
2025-01-12 00:40:51 +0000

    Tooltip custom - Añadido tooltip a la motosierra, bloque mágico y fresas

--- src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
+++ src/main/java/net/kaupenjoe/mccourse/item/ModItems.java
@@ -7,9 +7,14 @@ import net.kaupenjoe.mccourse.item.custom.ChainSawItem;
 import net.kaupenjoe.mccourse.item.custom.ModFoodComponents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+
+import java.util.List;
 
 public class ModItems {
 
@@ -19,7 +24,14 @@ public class ModItems {
     public static final Item CHAINSAW = registerItem("chainsaw",
             new ChainSawItem(new Item.Settings().maxDamage(32)));
     public static final Item STRAWBERRY = registerItem("strawberry",
-            new Item(new Item.Settings().food(ModFoodComponents.STRAWBERRY)));
+            new Item(new Item.Settings().food(ModFoodComponents.STRAWBERRY)){
+                @Override
+                public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
+                    tooltip.add(Text.translatable("tooltip.mccourse.strawberry.tooltip.1"));
+
+                    super.appendTooltip(stack, context, tooltip, type);
+                }
+            });
     public static final Item STARLIGHT_ASHES = registerItem("starlight_ashes",new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
