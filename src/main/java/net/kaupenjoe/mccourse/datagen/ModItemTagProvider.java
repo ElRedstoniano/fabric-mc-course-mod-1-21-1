8924472935c843416395f002a25f7833a11b71b8
ElRedstoniano <42276499+ElRedstoniano@users.noreply.github.com>
2025-03-26 19:00:59 +0000

    Armaduras custom - Trims - patrones en armadura custom

--- src/main/java/net/kaupenjoe/mccourse/datagen/ModItemTagProvider.java
+++ src/main/java/net/kaupenjoe/mccourse/datagen/ModItemTagProvider.java
@@ -6,6 +6,7 @@ import net.kaupenjoe.mccourse.item.ModItems;
 import net.kaupenjoe.mccourse.util.ModTags;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -22,5 +23,15 @@ public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
                 .add(Items.COAL)
                 .add(Items.STICK)
                 .add(Items.APPLE);
+
+        //getOrCreateTagBuilder(ItemTags.TRIMMABLE_ARMOR).add(ModItems.FLUORITE_HELMET) ...;
+        getOrCreateTagBuilder(ItemTags.HEAD_ARMOR)
+                .add(ModItems.FLUORITE_HELMET);
+        getOrCreateTagBuilder(ItemTags.CHEST_ARMOR)
+                .add(ModItems.FLUORITE_CHESTPLATE);
+        getOrCreateTagBuilder(ItemTags.LEG_ARMOR)
+                .add(ModItems.FLUORITE_LEGGINGS);
+        getOrCreateTagBuilder(ItemTags.FOOT_ARMOR)
+                .add(ModItems.FLUORITE_BOOTS);
     }
 }
