87ee399fe5eda35cc57c5da5c821369f341dc900
ElRedstoniano <42276499+ElRedstoniano@users.noreply.github.com>
2025-06-22 14:00:08 +0100

    Modelos dinámicos en entidad custom - Mostrando los cofres en el modelo de la entidad cuando son equipados

--- src/main/java/net/kaupenjoe/mccourse/entity/client/WarturtleModel.java
+++ src/main/java/net/kaupenjoe/mccourse/entity/client/WarturtleModel.java
@@ -101,9 +101,9 @@ public class WarturtleModel<T extends WarturtleEntity> extends SinglePartEntityM
         this.updateAnimation(entity.sittingAnimationState, WarturtleAnimations.ANIM_WARTURTLE_SITTING, ageInTicks, 1f);
         this.updateAnimation(entity.standingTransitionAnimationState, WarturtleAnimations.ANIM_WARTURTLE_EMERGE, ageInTicks, 1f);
 
-        tier1.visible = false;
-        tier2.visible = false;
-        tier3.visible = false;
+        tier1.visible = entity.hasTier1Chest();
+        tier2.visible = entity.hasTier2Chest();
+        tier3.visible = entity.hasTier3Chest();
     }
 
     public void setHeadAngles(float headYaw, float headPitch) {
